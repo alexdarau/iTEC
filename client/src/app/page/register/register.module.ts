3fd25466e1329b871d5929916aafbce444c08eb6
Flavia31 <ciui.alexandra31@gmail.com>
2022-05-28 18:14:55 +0300

    fix module error

--- client/src/app/page/register/register.module.ts
+++ client/src/app/page/register/register.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const routes: Routes = [
   { path: '', component: RegisterPageComponent }
@@ -13,6 +15,9 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
     RouterModule.forChild(routes)
   ],
   providers: [],
